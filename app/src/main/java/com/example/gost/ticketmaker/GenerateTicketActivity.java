dc3319f9d23e512831aafd91a627545a154ec171
Tyler Gosling <t.gosling61@gmail.com>
2019-11-21 14:47:11 -0500

    Edit Ticket onClick event handled

--- app/src/main/java/com/example/gost/ticketmaker/GenerateTicketActivity.java
+++ app/src/main/java/com/example/gost/ticketmaker/GenerateTicketActivity.java
@@ -15,22 +15,44 @@ public class GenerateTicketActivity extends AppCompatActivity {
 
     EditText timeStamp;
     EditText dateStamp;
+    EditText licenseET;
+    EditText provET;
+    EditText carManET;
+    EditText carModET;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_ticket);
         timeStamp = findViewById(R.id.timeET);
         dateStamp = findViewById(R.id.dateET);
+        licenseET = findViewById(R.id.plateNum);
+        provET = findViewById(R.id.provET);
+        carManET = findViewById(R.id.carManET);
+        carModET = findViewById(R.id.carModelET);
+
+
         SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy");
         String formatDate = date.format(new Date());
         SimpleDateFormat time = new SimpleDateFormat("hh:mm:ss");
         String formatTime = time.format(new Date());
         timeStamp.setText(formatTime);
         dateStamp.setText(formatDate);
+        timeStamp.setEnabled(false);
+        dateStamp.setEnabled(false);
+        licenseET.setEnabled(false);
+        carManET.setEnabled(false);
+        carModET.setEnabled(false);
+        provET.setEnabled(false);
+
     }
 
     public void onBackToMainClick(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void onEditTicClick(View view){
+        carManET.setEnabled(true);
+        carModET.setEnabled(true);
     }
 }
