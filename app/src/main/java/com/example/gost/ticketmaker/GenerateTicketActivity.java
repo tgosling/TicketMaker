beb0beba005e785306897dfd663004be45897201
Tyler Gosling <t.gosling61@gmail.com>
2019-11-20 23:43:45 -0500

    General layout/design for app

--- app/src/main/java/com/example/gost/ticketmaker/GenerateTicketActivity.java
+++ app/src/main/java/com/example/gost/ticketmaker/GenerateTicketActivity.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class GenerateTicketActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_generate_ticket);
     }
 
-    public void onGenTickClick(View view){
-        Intent intent = new Intent(this, GenerateTicketActivity.class);
+    public void onBackToMainClick(View view){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
