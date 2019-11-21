0678882838f6b5159c51493a20a3a277ed28a019
Tyler Gosling <t.gosling61@gmail.com>
2019-11-21 14:05:28 -0500

    Time stamp and date for ticket

--- app/src/main/java/com/example/gost/ticketmaker/GenerateTicketActivity.java
+++ app/src/main/java/com/example/gost/ticketmaker/GenerateTicketActivity.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class GenerateTicketActivity extends AppCompatActivity {
 
+    EditText timeStamp;
+    EditText dateStamp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_ticket);
+        timeStamp = findViewById(R.id.timeET);
+        dateStamp = findViewById(R.id.dateET);
+        SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy");
+        String formatDate = date.format(new Date());
+        SimpleDateFormat time = new SimpleDateFormat("hh:mm:ss");
+        String formatTime = time.format(new Date());
+        timeStamp.setText(formatTime);
+        dateStamp.setText(formatDate);
     }
 
     public void onBackToMainClick(View view){
