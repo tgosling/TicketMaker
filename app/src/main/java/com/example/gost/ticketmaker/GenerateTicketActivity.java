e6c115a73e97aec5ca6ab84bf7872aaeb29a1209
Tyler Gosling <t.gosling61@gmail.com>
2019-12-01 13:07:22 -0500

    added firebase test code for image

--- app/src/main/java/com/example/gost/ticketmaker/GenerateTicketActivity.java
+++ app/src/main/java/com/example/gost/ticketmaker/GenerateTicketActivity.java
@@ -1,8 +1,7 @@
 package com.example.gost.ticketmaker;
 
-import androidx.annotation.ColorRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.Context;
@@ -10,10 +9,11 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,13 +22,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
 import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
-import static android.graphics.BlendMode.COLOR;
 
 public class GenerateTicketActivity extends AppCompatActivity {
 
@@ -38,12 +46,18 @@ public class GenerateTicketActivity extends AppCompatActivity {
     EditText provET;
     EditText carManET;
     EditText carModET;
+    EditText tickID;
     ImageView imageView;
+    Spinner spinner;
+    Bitmap licBitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_ticket);
+        //makes app portrait only
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //adds back bar to main
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
@@ -54,30 +68,71 @@ public class GenerateTicketActivity extends AppCompatActivity {
         carManET = findViewById(R.id.carManET);
         carModET = findViewById(R.id.carModelET);
         imageView = findViewById(R.id.imageView);
-
-        Spinner spinner = (Spinner) findViewById(R.id.infrac_spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
+        spinner = findViewById(R.id.infrac_spinner);
+        tickID = findViewById(R.id.ticIDET);
+        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.infrac_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+        // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+        //provide unique ID for ticket
+        //String uniqueID = UUID.randomUUID().toString();
+        //tickID.setText(uniqueID);
+        //Random rand = new Random();
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 1000+1);
+        //double x = (int)Math.random();
+       // tickID.setText(Integer.toString(randomNum));
 
-
+        //get date and time for ticket
         SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy");
         String formatDate = date.format(new Date());
-        SimpleDateFormat time = new SimpleDateFormat("hh:mm:ss");
+        SimpleDateFormat time = new SimpleDateFormat("kk:mm:ss");
         String formatTime = time.format(new Date());
         timeStamp.setText(formatTime);
         dateStamp.setText(formatDate);
+        //disable text fields
         timeStamp.setEnabled(false);
         dateStamp.setEnabled(false);
         licenseET.setEnabled(false);
         carManET.setEnabled(false);
         carModET.setEnabled(false);
         provET.setEnabled(false);
+        tickID.setEnabled(false);
+
+
+        /*TESTING*/
+        //get initial bitmap
+        licBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.license
+        );
+        //using initial bitmap as dataset
+        FirebaseVisionImage fvImage = FirebaseVisionImage.fromBitmap(licBitmap);
+        //the on-device model for text-recognition
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+        //pass the image to the processImage method
+        Task<FirebaseVisionText> result = detector.processImage(fvImage)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                        // Task completed successfully
+                        // Display the text found in the textView
+                        tickID.setText(firebaseVisionText.getText());
+                    }
+                })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                // ...
+                                tickID.setText("task failed");
+                            }
+                        });
+
     }
 
     public void onBackToMainClick(View view){
@@ -85,6 +140,7 @@ public class GenerateTicketActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //Allow non-editable fields to be editable if incorrect data
     public void onEditTicClick(View view){
         carManET.setEnabled(true);
         carModET.setEnabled(true);
@@ -129,12 +185,41 @@ public class GenerateTicketActivity extends AppCompatActivity {
                 //Thumbnail
                 Bundle extras = data.getExtras();
                 Bitmap imageBitmap = (Bitmap) extras.get("data");
-                imageView.setImageBitmap(imageBitmap);
+                Matrix matrix = new Matrix();
+                matrix.postRotate(90);
+                Bitmap rotatedImage = Bitmap.createBitmap(imageBitmap, 0,0, imageBitmap.getWidth(), imageBitmap.getHeight(), matrix, true);
+                imageView.setImageBitmap(rotatedImage);
 
                 //Actual file
                 Uri tempUri = getImageUri(getApplicationContext(), imageBitmap);
                 File finalFile = new File(getRealPathFromURI(tempUri));
                 System.out.println(tempUri);
+
+                /*TESTING*/
+                //using initial bitmap as dataset
+                FirebaseVisionImage fvImage = FirebaseVisionImage.fromBitmap(imageBitmap);
+                //the on-device model for text-recognition
+                FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+                //pass the image to the processImage method
+                Task<FirebaseVisionText> result = detector.processImage(fvImage)
+                        .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                                // Task completed successfully
+                                // Display the text found in the textView
+                                tickID.setText(firebaseVisionText.getText());
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                        tickID.setText("task failed");
+                                    }
+                                });
+
             }catch(Exception e){
                 String what = e.getMessage();
             }
@@ -145,22 +230,5 @@ public class GenerateTicketActivity extends AppCompatActivity {
         Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
         startActivityForResult(myIntent, 0);
         return true;
-    }
-
-    String currentPhotoPath;
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
     }
 }
--- app/src/main/java/com/example/gost/ticketmaker/ViewTicketsActivity.java
+++ app/src/main/java/com/example/gost/ticketmaker/ViewTicketsActivity.java
@@ -12,6 +12,8 @@ public class ViewTicketsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_tickets);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     public void onBackToMainClick(View view) {
